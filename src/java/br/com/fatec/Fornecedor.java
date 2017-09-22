e0b0a9c0bb76e5ef9c17775db1cf5750dc41e4fd
ravan <ravan@DESKTOP-TJJHRF1.home>
2017-09-22 10:31:37 -0300

    3°commit: Novo formulario dentro da pagina do fornecedor, novas vairiaveis na classe fornecedor, tambem feito encapsulamento das variveis (get e set)

--- src/java/br/com/fatec/Fornecedor.java
+++ src/java/br/com/fatec/Fornecedor.java
@@ -1,14 +1,65 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.fatec;
 
-/**
- *
- * @author ravan
- */
 public class Fornecedor {
+    private String nome;
+    private String r_social;
+    private String cnpj;
+    private String email;
+    private String telefone;
+    private String endereço;
     
+    //encapsulamento dos dados
+    //encapsulamento do nome
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    //encapsulamento da razão social
+    public String getR_social() {
+        return r_social;
+    }
+
+    public void setR_social(String r_social) {
+        this.r_social = r_social;
+    }
+    
+    //encapsulamento do cnpj
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+    
+    //encapsulamento do email
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    //encapsulamento do telefone
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+    //encapsulamento do endereço
+    public String getEndereço() {
+        return endereço;
+    }
+
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
 }
